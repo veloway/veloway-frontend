055087ed2a50c6a94db94e7c8b2ac62cbd1e286c
JoaquinB3 <joaquinbotteri3@gmail.com>
2024-09-30 15:11:18 -0300

    feat: Agrega una tabla

--- src/app/driver/page.tsx
+++ src/app/driver/page.tsx
@@ -2,14 +2,36 @@
 import ContainerFlex from "@/components/ui/container-flex/ContainerFlex";
 import Header from "@/components/ui/header/Header";
 import SinViaje from "@/components/driver/sinViaje/SinViaje"
+import Table from "@/components/ui/table/Table";
 
 export default function DriverHomePage() {
+
+  const columns = ["Numero", "Fecha y hora", "Origen", "Destino", "Monto"];
+  const data = [
+    {
+      nro: 2,
+      fecha: "28/05/2024 15:33",
+      origen: "Plaza Italia",
+      destino: "UTN La Plata",
+      monto: 500
+    },
+    {
+      nro: 2,
+      fecha: "21/09/2024",
+      origen: "Plaza Moreno",
+      destino: "UTN La Plata",
+      monto: 300
+    }
+  ]
+
   return (
     <div>
       <Header/>
       <ContainerFlex>
           <SinViaje/>
-          
+      </ContainerFlex>
+      <ContainerFlex>
+          <Table title="Historial de viajes" columns={columns} data={data}/>
       </ContainerFlex>
     </div>
   )
--- src/components/ui/table/Table.tsx
+++ src/components/ui/table/Table.tsx
@@ -1,4 +1,3 @@
-
 type Data = Object[];
 
 interface TableProps {
@@ -7,11 +6,35 @@ interface TableProps {
     data: Data;
 }
 
-export default function Table({}:TableProps) {
+export default function Table({ title, columns, data}:TableProps) {
 
     return (
-        <div>
-            
+        <div className="m-8">
+            <h3 className="text-lg">{title}</h3>
+            <table className="w-[1300px] bg-[var(--color-quaternary)] ">
+                <thead>
+                    <tr>
+                        {
+                            columns.map((column, index) => (
+                                <th key={index}>
+                                    {column}
+                                </th>
+                            ))
+                        } 
+                    </tr>
+                </thead>
+                <tbody>
+                {data.map((data, index) => (
+                        <tr key={index}>
+                            {columns.map((column, colIndex) => (
+                                <td key={colIndex}>
+                                    {}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
