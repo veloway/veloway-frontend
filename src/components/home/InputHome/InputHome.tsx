d6cd5c9bbe5bee31311c82093e70e9a8039bf489
geraltamirano22 <geraltamirano10@gmail.com>
2024-10-10 19:50:02 -0300

    Agrega funcionalidad input, y corrige visual boton header y botones del medio

--- src/components/home/InputHome/InputHome.tsx
+++ src/components/home/InputHome/InputHome.tsx
@@ -4,25 +4,16 @@ import { FaArrowCircleRight } from "react-icons/fa";
 import './InputHome.css'
 
 export function InputHome () {
-    const [inputValue, setInputValue] = useState('');
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue (e.target.value);
-    }
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault(); // Evita que el formulario recargue la página
+    /*const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
         console.log("Codigo de seguimiento recibido: ", inputValue);
-        // Lógica adicional para manejar la sumisión del formulario
-      };
+      };*/
 
     return(
-        <form className="form-contenedor" onSubmit={handleSubmit}>
+        <form className="form-contenedor" /*onSubmit={handleSubmit}*/>
             <input 
                 className="input-home"
                 type="text" 
-                value={inputValue} 
-                onChange={handleInputChange} 
                 placeholder="Ingrese numero de seguimiento" 
                 style={{ color: "black"}}
             />
