560e3c79cf17c3a1727a72a95973ff2bf37335be
geraltamirano22 <geraltamirano10@gmail.com>
2024-12-04 19:28:10 -0300

    feat: Agrega contenedor con los detalles del envio

--- src/components/trackcode/ViewTC/ViewTC.tsx
+++ src/components/trackcode/ViewTC/ViewTC.tsx
@@ -3,17 +3,14 @@ import ProgressBar from "../ProgressBar/ProgressBar";
 
 const steps = [
     { label: "Retirado", isActive: true },
-    { label: "Llegó a nuestra sucursal", isActive: true },
-    { label: "Visita a domicilio", isActive: true },
-    { label: "Entregado", isActive: true },
+    { label: "Llegó a nuestra sucursal", isActive: false },
+    { label: "Visita a domicilio", isActive: false },
+    { label: "Entregado", isActive: false },
   ];
 
-  const lastActiveStep = steps.filter((step) => step.isActive).pop();
+const lastActiveStep = steps.filter((step) => step.isActive).pop();
 
-  const lastActiveStepLabel = lastActiveStep ? lastActiveStep.label : "No iniciado";
-
-
-
+const lastActiveStepLabel = lastActiveStep ? lastActiveStep.label : "No iniciado";
 
 export default function viewtc(){
     return (
@@ -33,10 +30,25 @@ export default function viewtc(){
                     ))}
                 </div>
             </div>
+            
             <div className="display: flex justify-around">
                 <CardTC textoTit="Codigo de seguimiento:" textoCuerpo="2364736523"></CardTC>
                 <CardTC textoTit="Estado del envio:" textoCuerpo={lastActiveStepLabel}></CardTC>
             </div>
+
+            <div className="display: flex justify-around m-16">
+                <div className="grid grid-rows-auto text-center justify-center outline outline-blue-500 rounded-md p-4  w-1/2 bg-blue-100">
+                    <p className="font-bold">Origen: </p>
+                    <p className="mb-4">Honorio Pueyrredon 1841, Capital Federal - BUENOS AIRES</p>
+                    <p className="font-bold">Destino: </p>
+                    <p className="mb-4">443 3283, City Bell - BUENOS AIRES</p>
+                    <p className="font-bold">Monto: </p>
+                    <p className="mb-4">$98.000</p>
+                    <p className="font-bold">Peso (gr): </p> 
+                    <p>6537</p>
+                </div>
+            </div>
+            
         </>
     );
 }
